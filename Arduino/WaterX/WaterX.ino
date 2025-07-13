9b132db6b64cd542417eb4885ef6f3b10050fd25
Allen Gabrielle Cruiz <cruizallen2@gmail.com>
2025-07-13 17:10:34 +0800

    feat: Add icon definitions for water level monitoring and update server port

--- Arduino/WaterX/WaterX.ino
+++ Arduino/WaterX/WaterX.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_SSD1306.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include "icons.h"
 
 #define VERSION "0.1.0"
 
@@ -23,7 +24,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const char* ssid = "POGO";          
 const char* password = "MjAwMzA1";   
-ESP8266WebServer server(80);
+ESP8266WebServer server(65500);
 
 long duration;
 float distanceCm;
@@ -31,31 +32,6 @@ int waterPercent;
 String waterStatus;
 unsigned long lastBuzzerTime = 0;
 bool buzzerState = false;
-
-// Bitmap icons (8x8 pixels)
-const unsigned char PROGMEM water_drop[] = {
-  0x18, 0x18, 0x3c, 0x3c, 0x7e, 0x7e, 0x3c, 0x18
-};
-
-const unsigned char PROGMEM tank_low[] = {
-  0xff, 0x81, 0x81, 0x81, 0x81, 0x81, 0xff, 0xff
-};
-
-const unsigned char PROGMEM tank_med[] = {
-  0xff, 0x81, 0x81, 0xff, 0xff, 0xff, 0xff, 0xff
-};
-
-const unsigned char PROGMEM tank_high[] = {
-  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-};
-
-const unsigned char PROGMEM warning[] = {
-  0x18, 0x3c, 0x7e, 0x7e, 0x18, 0x18, 0x00, 0x18
-};
-
-const unsigned char PROGMEM wifi_icon[] = {
-  0x00, 0x3c, 0x42, 0x18, 0x24, 0x00, 0x18, 0x00
-};
 
 void setup() {
   Serial.begin(115200);
@@ -95,7 +71,6 @@ void setup() {
 }
 
 void loop() {
-  // Handle web requests only (remove DNS server)
   server.handleClient();
   
   digitalWrite(trigPin, LOW);
@@ -107,65 +82,59 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distanceCm = duration * SOUND_VELOCITY / 2;
 
-  // Calculate water level percentage
+
   waterPercent = constrain(100 - (distanceCm / MAX_DEPTH_CM * 100), 0, 100);
 
-  // Display on OLED
   display.clearDisplay();
   
-  // Title with water drop icon
-  display.drawBitmap(0, 0, water_drop, 8, 8, SSD1306_WHITE);
+  display.drawBitmap(0, 0, water_drop, ICON_WIDTH, ICON_HEIGHT, SSD1306_WHITE);
   display.setCursor(12, 0);
   display.setTextSize(1);
-  display.println("WaterX Monitor");
+  display.println("WaterX");
   
-  // WiFi status - show connection info
-  display.drawBitmap(0, 10, wifi_icon, 8, 8, SSD1306_WHITE);
+  display.drawBitmap(0, 10, wifi_icon, ICON_WIDTH, ICON_HEIGHT, SSD1306_WHITE);
   display.setCursor(12, 10);
   display.print("IP: ");
   display.println(WiFi.localIP());
   
-  // Distance
   display.setCursor(0, 20);
   display.print("Dist: ");
   display.print(distanceCm, 1);
   display.println("cm");
   
-  // Water level with tank icon
   display.setCursor(0, 32);
   display.print("Level: ");
   display.print(waterPercent);
   display.print("%");
   
-  // Tank icon based on level and active buzzer control
   if (waterPercent < 50) {
-    display.drawBitmap(80, 32, tank_low, 8, 8, SSD1306_WHITE);
+    display.drawBitmap(80, 32, tank_low, ICON_WIDTH, ICON_HEIGHT, SSD1306_WHITE);
     display.setCursor(0, 48);
     display.print("LOW WATER");
     
     waterStatus = "LOW";
-    // No buzzer for low water
+
     digitalWrite(buzzerPin, LOW);
     Serial.println(" - LOW WATER");
   } else if (waterPercent < 90) {
-    display.drawBitmap(80, 32, tank_med, 8, 8, SSD1306_WHITE);
+    display.drawBitmap(80, 32, tank_med, ICON_WIDTH, ICON_HEIGHT, SSD1306_WHITE);
     display.setCursor(0, 48);
     display.print("MEDIUM WATER");
     
     waterStatus = "MEDIUM";
-    // Single beep for medium water (warning)
+
     digitalWrite(buzzerPin, HIGH);
     delay(100);
     digitalWrite(buzzerPin, LOW);
     Serial.println(" - MEDIUM WATER");
   } else {
-    display.drawBitmap(80, 32, tank_high, 8, 8, SSD1306_WHITE);
+    display.drawBitmap(80, 32, tank_high, ICON_WIDTH, ICON_HEIGHT, SSD1306_WHITE);
     display.setCursor(0, 48);
     display.print("HIGH WATER");
-    display.drawBitmap(70, 48, warning, 8, 8, SSD1306_WHITE);
+    display.drawBitmap(70, 48, warning, ICON_WIDTH, ICON_HEIGHT, SSD1306_WHITE);
     
     waterStatus = "HIGH";
-    // Continuous beeping for high water (flood alert)
+    
     if (millis() - lastBuzzerTime > (buzzerState ? BUZZER_ON_TIME : BUZZER_OFF_TIME)) {
       buzzerState = !buzzerState;
       digitalWrite(buzzerPin, buzzerState ? HIGH : LOW);
@@ -195,7 +164,8 @@ void handleReadings() {
 void handleStatus() {
   String json = "{";
   json += "\"device\":\"WaterX Monitor\",";
-  json += "\"version\":\"1.0\",";
+  json += "\"version\":\"" + String(VERSION) + "\",";
+  json += "\"iconsVersion\":\"" + String(ICONS_VERSION) + "\",";
   json += "\"uptime\":" + String(millis()) + ",";
   json += "\"connectedClients\":" + String(WiFi.softAPgetStationNum());
   json += "}";
@@ -212,8 +182,8 @@ void handleNotFound() {
   response += " \\        /  / __ \\|  | \\  ___/|  | \\/     \\ \n";
   response += "  \\__/\\  /  (____  /__|  \\___  >__| /___/\\  \\\n";
   response += "       \\/        \\/          \\/           \\_/\n";
-  response += "                                       By: AlienWolfX"
-  response += "                                       Version: v" + String(VERSION) + "\n";
+  response += "                                 By: AlienWolfX\n";
+  response += "                                 Version: v" + String(VERSION) + "\n";
   
   server.send(404, "text/plain", response);
 }
